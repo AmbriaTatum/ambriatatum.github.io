22f4279143ab1ece39f92f32e271b172be37c953
Ambria Tatum <24684110+AmbriaTatum@users.noreply.github.com>
2021-04-23 20:43:19 -0500

    updating the index to keep my streak
    day 3 has been a crappy day, so keeping it simple and adding a crappy form.

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const element = (
     Welcome to my site!
   </h1>
   );
+
+class MyForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>I'm glad you could make it!</h1>
+        <p>Since we're getting to know one another, could you please enter your name?</p>
+        <input
+          type="text"
+        />
+      </form>
+    );
+  }
+}
+ReactDOM.render(<MyForm />, document.getElementById('root'));
